d54faa359f4c1cdf45c5ed432974364f2fedac97
Kumari Thotapalli <t.kumari59@gmail.com>
2023-11-30 11:45:53 +0100

    updated components

---
+++ src/components/SiteNavigation.vue
@@ -0,0 +1,19 @@
+<template>
+    <header class="sticky top-0 bg-weather-primary shadow-lg">
+        <nav class="container flex flex-col sm:flex-row items-center gap-4 text-white py-6">
+            <div class="flex flex-1 items-center gap-3">
+                <i class="fa-solid fa-sun text-2xl"></i>
+                <p class="text-2xl">The Local Weather</p>
+                 
+            </div>
+
+        </nav>
+
+    </header>
+</template>
+
+<script setup>
+
+</script>
+
+
